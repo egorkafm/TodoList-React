ff65e5ffffcebad601585d7166aeb8e9d8eccfc2
Yehor Lachchenko <lashchenko.ys@gmail.com>
2021-01-26 17:32:48 +0200

    add loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: "Купить  хлеб" },
-    { id: 2, completed: false, title: "Купить  масло" },
-    { id: 3, completed: false, title: "Купить  молоко" },
-  ]);
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
 
   function toggleTodo(id) {
     setTodos(
@@ -42,9 +51,11 @@ function App() {
       <div className="wrapper">
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo} />
+
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos!</p>
         )}
       </div>
---
+++ src/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+  <div className="lds-spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
